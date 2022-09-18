5a5a13a83cede61b73c4d10513d18d1cefb64e0c
bhavuk2002 <vvbnmittal@gmail.com>
2022-09-18 16:17:22 +0530

    added frontend pages

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
@@ -15,33 +14,15 @@ const userSchema = new mongoose.Schema({
         trim: true,
         unique: true,
         lowercase: true,
-        // validate(value){
-        //     if(!validator.isEmail(value)){
-        //         throw new Error('Email is invalid')
-        //     }
-        // }
     },
     age: {
         type: Number,
         default: 0,
-        // validate(value){
-        //     if(value < 0){
-        //         throw new Error('Age must be a positive number') 
-        //     }
-        // }
     },
     password: {
         type: String,
         trim: true,
         minlength: 7,
-        // validate(value){
-        //     // if(value.length < 7){
-        //     //     throw new Error('Password must be greater than 6 digits')
-        //     // } or we can use minlength
-        //     if(value.toLowerCase().includes('password')){
-        //         throw new Error('Password must not contain password in it')
-        //     }
-        // }
     },
     tokens: [{
         token: {
